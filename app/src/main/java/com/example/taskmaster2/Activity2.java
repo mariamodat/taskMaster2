b761e7a89f5dd5bc22097ec5256128f8ff708c19
mariamodat <mariamodat0@gmail.com>
2021-08-09 00:16:44 +0300

    edited styling

--- app/src/main/java/com/example/taskmaster2/Activity2.java
+++ app/src/main/java/com/example/taskmaster2/Activity2.java
@@ -3,6 +3,9 @@ package com.example.taskmaster2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+      Button submitB= (Button) findViewById(R.id.addTask) ;
+
+      submitB.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        Toast toast= Toast.makeText( getApplicationContext(),"Submitted!" , Toast.LENGTH_SHORT);
+        toast.show();
+        }
+      });
     }
+
+  public void submitTask(){
+    Toast.makeText(Activity2.this,"Submitted" , Toast.LENGTH_LONG).show();
+  }
+
 }
--- app/src/main/java/com/example/taskmaster2/MainActivity.java
+++ app/src/main/java/com/example/taskmaster2/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     Button addBtn = (Button) findViewById(R.id.addBtn);
+//    Button submitB= (Button) findViewById(R.id.submitBtn) ;
+//    submitB.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View v) {
+//        submitTask();
+//      }
+//    });
       addBtn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -25,5 +33,12 @@ openNewPage();
   public void openNewPage(){
     Intent intent = new Intent(MainActivity.this, Activity2.class);
     startActivity(intent);
+//    Toast.makeText(MainActivity.this,"Submitted" , Toast.LENGTH_LONG).show();
   }
+
+//public void submitTask(){
+//  Toast.makeText(MainActivity.this,"Submitted" , Toast.LENGTH_LONG).show();
+//}
+
+
 }
